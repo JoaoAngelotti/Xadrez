1eb8c4a56a57e3dc11608ca85591b7439983d028
Joao Angelotti <jpinto1@lenovo.com>
2022-07-04 09:01:33 -0300

    Tratando erros com exceção personalizada

---
+++ Projeto de Xadrez/Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_de_Xadrez.Tabuleiro
+{
+    class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+            
+        }
+    }
+}
